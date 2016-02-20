406a5c55fd73b50aa011440e076b8c1a63be1f4a
Andrew Welch <ajwpr5@gmail.com>
2016-02-20 23:40:46 +0000

    drag drop

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,56 @@
 jQuery(document).ready(function($) {
-var filterParams = '';
+    $(".draggable").draggable({
+        revert: true,
+        start: function() {
+            console.log('start');
+        },
+        drag: function() {
+            console.log('draggoing');
+        },
+        stop: function() {
+            console.log('stop');
+        }
+    });
+    $(".droppable").droppable({
+        activeClass: "ui-state-default",
+        hoverClass: "ui-state-hover",
+        drop: function(event, ui) {
+            $(this).addClass("ui-state-highlight");
+            // console.log(ui.draggable.text());
+            console.log(ui.draggable.text());
+            // console.log(ui.draggable.data('postid'));
+            // console.log($(this).data('termid'));
 
-console.log('working js');
-        $.ajax({
-            url: mnt_do_sorting_script.ajaxurl,
-            type: 'GET',
-            data: ({
-                action: 'mnt_do_sorting',
-                filterParams: filterParams
-            }),
-            success: function(data) {
+
+            var postid = ui.draggable.data('postid');
+            var termid = $(this).data('termid');
+            var termslug = $(this).data('termslug');
+
+             var filterParams = {};
+             filterParams.postid = postid;
+             filterParams.termid = termid;
+             filterParams.termslug = termslug;
+
+             console.log(filterParams);
+
+            $.ajax({
+                url: mnt_drop_action.ajaxurl,
+                type: 'GET',
+                data: ({
+                    action: 'mnt_drop_action',
+                    filterParams: filterParams
+                }),
+                success: function(data) {
+                    console.log(data);
+
+
+                },
+                error: function(data) {}
+            });
 
 
 
-
-            },
-            error: function(data) {}
-        });
-
-
+        }
+    });
+    console.log('working js');
 });
